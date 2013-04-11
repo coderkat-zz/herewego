e50106f6b681f13e0ebed3a7f7ff7165dfcf9d60
Kat <kat@kat.(none)>
2013-04-11 16:16:30 -0700

    front end up, preference buttons not yet working

--- herewego.py
+++ herewego.py
@@ -1,52 +1,65 @@
+from flask import Flask, render_template, redirect, request, session
+from flask import url_for, g, flash
 import model
-import sqlalchemy.exc
-import feedparser # python library that parses feeds in RSS, Atom, and RDF
+from model import session as db_session, Users, Stories, Preferences
 
-# new york times rss for their news homepage
-nyt = 'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml'
-
-# npr rss news headlines
-npr = 'http://www.npr.org/rss/rss.php?id=1001'
-
-# bbc news rss top stories
-bbc = 'http://feeds.bbci.co.uk/news/rss.xml'
-
-#cnn rss top stories - will need extra parsing for 
-# description from feedparser, they build it in with tons of 
-# other shit
-cnn = 'http://rss.cnn.com/rss/cnn_topstories.rss'
+app = Flask(__name__)
+app.secret_key = "bananabananabanana"
 
 
-# use with NYT and CNN!
-# grabs 'description' from feedparser and parses out actual
-# abstract
-# TODO: FIGURE OUT WHERE THIS FITS IN TO DB CONSTRUCTION
-def isolate_desc(rss):
-	description = (feedparser.parse(rss).entries[0].description.split('<'))[0]
-	return description
 
 
-# get a list of titles in rss feed
-def titles(rss):
-	titles = []
-	# parse rss file
-	parsed = feedparser.parse(rss)
-	# return list of article titles from rss feed
-	for i in range(len(parsed.entries)):
-		titles.append(parsed.entries[i].title)
-	return titles
+@app.route("/")
+def index():
+	return render_template("index.html")
 
-# get list of urls 
-def urls(rss):
-	urls = []
-	parsed = feedparser.parse(rss)
-	# return list of article urls from rss feed
-	for i in range(len(parsed.entries)):
-		urls.append(parsed.entries[i].link)
-	print urls
+@app.route("/login", methods=["GET"])
+def login():
+	return render_template("login.html")
 
-def main():
-	urls(nyt)
+@app.route("/validate", methods=['POST'])
+def validate_login():
+	# TODO: GO THROUGH AND SANITIZE STUFF --> will need to encode form input to match what's encoded in the database (using urllib.quote())
+	form_email = request.form['email']
+	form_password = request.form['password']
+
+	#form_email and form_password must both exist and match in db for row to be an object. Row is the entire row from the users table, including the id
+	row = model.session.query(model.Users).filter_by(email=form_email, password=form_password).first()
+
+	if row: 		
+		session['email'] = request.form['email']
+		session['user_id'] = row.id
+		flash('Logged in as: ' + session['email'])
+		return redirect("/news")		
+	else:
+		flash('Please enter a valid email address and password.')
+		return redirect("/login") 
+
+@app.route("/news")
+def news():
+	# TODO: make this more random, pull from various sources, etc...
+	stories_list = model.session.query(model.Stories).limit(10).all()
+	return render_template("news.html", story_list = stories_list)
+
+@app.route("/like", methods=["POST"])
+def like():
+	story = request.form("story_id")
+	new_like = model.Preferences(story_id=story, user_id=session['user_id'], preference=1)
+
+	model.session.add(new_like)
+	model.session.commit()
+	return redirect(url_for('news'))
+
+
+@app.route("/dislike", methods=["POST"])
+def dislike():
+	story = request.form("story_id")
+	new_dislike = model.Preferences(story_id=story, user_id=session['user_id'], preference=0)
+
+	model.session.add(new_dislike)
+	model.session.commit()
+	return redirect(url_for('news'))
+
 
 if __name__ == "__main__":
-	main()
+	app.run(debug = True)
--- seed.py
+++ seed.py
@@ -2,49 +2,35 @@ import model
 import sqlalchemy.exc
 import feedparser # python library that parses feeds in RSS, Atom, and RDF
 
-# new york times rss for their news homepage
-nyt = 'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml'
-
-# npr rss news headlines
-npr = 'http://www.npr.org/rss/rss.php?id=1001'
-
-# bbc news rss top stories
-bbc = 'http://feeds.bbci.co.uk/news/rss.xml'
-
-#cnn rss top stories - will need extra parsing for 
-# description from feedparser, they build it in with tons of 
-# other shit
-cnn = 'http://rss.cnn.com/rss/cnn_topstories.rss'
-
-
-# use with NYT and CNN!
-# grabs 'description' from feedparser and parses out actual
-# abstract
-# TODO: FIGURE OUT WHERE THIS FITS IN TO DB CONSTRUCTION
-def isolate_desc(rss):
-	description = (feedparser.parse(rss).entries[0].description.split('<'))[0]
-	return description
-
+sources = {"New York Times":'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml', "NPR News":'http://www.npr.org/rss/rss.php?id=1001', "BBC":'http://feeds.bbci.co.uk/news/rss.xml', "CNN":'http://rss.cnn.com/rss/cnn_topstories.rss'}
 
 # seed db! open rss file, read it, parse it, create object, 
 # add obj to session, commit, and repeat til done
-#TODO: GET THIS TO WORK?!
+
 def load_stories(source, session):
-	parsed = feedparser.parse(source)
-	print parsed
+	# use feedparser to grab & parse the rss feed
+	parsed = feedparser.parse(sources[source])
+	# go through each entry in the RSS feed to pull out elements for Stories
 	for i in range(len(parsed.entries)):
 		title = parsed.entries[i].title
 		url = parsed.entries[i].link
+		source = source
+		# pull abstract, parse out extra crap that is sometimes included 
 		abstract = (parsed.entries[i].description.split('<'))[0]
-		# story = model.Stories(title=title, url=url, abstract=abstract)
-		# session.add(user)
-		# session.commit()
-
-def load_tags(session):
+		# connect with db
+		story = model.Stories(title=title, url=url, abstract=abstract, source=source)
+		# add story to db
+		session.add(story)
+		# commit 
+		session.commit()
+		
+def load_tags(source, session):
 	pass
 
-def main():
-	load_stories(npr, session)
+def main(session):
+	for item in sources:
+		load_stories(item, session)
 
 if __name__ == "__main__":
-	main()
+	s = model.session
+	main(s)
